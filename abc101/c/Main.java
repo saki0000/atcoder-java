227a90bcc411964480b8c7a0f9df096c39490435
saki000 <hangchengtengqi@gmail.com>
2023-07-04 13:37:56 +0900

    abc101

---
+++ abc101/c/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        try(Scanner scan = new Scanner(System.in)){
+          System.out.println();
+        }
+    }
+}
